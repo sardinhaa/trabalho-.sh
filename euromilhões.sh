51eef9c1ea44477e0417189384166e9ba82c3329
gonçalo Pereira <128597095+sardinhaa@users.noreply.github.com>
2023-10-30 23:16:13 -0100

    Update euromilhões.sh

--- euromilhões.sh
+++ euromilhões.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
-while true; do
-    echo "Chave do EuroMilhões:"
+rpt="s"
 
-    echo -n "Números: "
-    for ((i = 1; i <= 5; i++)); do
-    numero=$(( RANDOM % 50 + 1))
-    echo -n "$numero "
-    done
+while [ "$rpt" = "s" ]; do
+        echo "aqui esta a chave:"
+        echo -n "numeros: "
+        for ((i = 1; i <= 5; i++)); do
+            numero=$((RANDOM % 50 + 1))
+            echo -n "$numero "
+        done
 
-    echo -n "Estrelas: "
-    for ((i = 1; i <= 2; i++)); do
-    estrela=$((1 + RANDOM % 12))
-    echo -n "$estrela "
-    done
+        echo -n "estrelas: "
+        for ((i = 1; i <= 2; i++)); do
+            estrelas=$((RANDOM % 12 + 1))
+            echo -n "$estrelas "
+        done
 
-    echo
+        echo
 
-    read -p "Deseja gerar outra chave? (S para sim, qualquer tecla para sair): " resposta
-    if [ "$resposta" != "S" ] && [ "$resposta" != "s" ]; then
-    break
-    fi
+    read -p "queres gerar outra chave para o euromilhoes? (s/n): " rpt
 done
+
+echo "adeus"
